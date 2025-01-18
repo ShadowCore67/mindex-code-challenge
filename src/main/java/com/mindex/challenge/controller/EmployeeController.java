95a35ee0d4a35ca01cd67c29905acce6f484c0f8
mindex <dev@mindex.com>
2025-01-18 17:20:02 -0500

    compenation endpoints/logging/error handling

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -1,40 +1,58 @@
 package com.mindex.challenge.controller;
 
+import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.CompensationService;
 import com.mindex.challenge.service.EmployeeService;
+
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
     private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
 
     @Autowired
     private EmployeeService employeeService;
 
-    @PostMapping("/employee")
+    @Autowired
+    private CompensationService compensationService;
+
+    @PostMapping
     public Employee create(@RequestBody Employee employee) {
         LOG.debug("Received employee create request for [{}]", employee);
 
         return employeeService.create(employee);
     }
 
-    @GetMapping("/employee/{id}")
-    public Employee read(@PathVariable String id) {
-        LOG.debug("Received employee create request for id [{}]", id);
+    @GetMapping("/{id}")
+    public ResponseEntity read(@PathVariable String id) {
+        LOG.debug("Received employee read request for id [{}]", id);
 
-        return employeeService.read(id);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(employeeService.read(id));
+        }
+        catch(RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
-    @PutMapping("/employee/put/{id}")
+    @PutMapping("/{id}")
     public Employee update(@PathVariable String id, @RequestBody Employee employee) {
         LOG.debug("Received employee create request for id [{}] and employee [{}]", id, employee);
 
@@ -42,20 +60,55 @@ public class EmployeeController {
         return employeeService.update(employee);
     }
 
-    //Including this endpoint in the employee controller instead of a separate controller,
-    //as this is still very much an employee-based query in my opinion
-    @GetMapping("/employee/reportStructure/{id}")
-    public ReportingStructure getReportingStructure(@PathVariable String id) {
+    //Including these endpoints in the employee controller instead of a separate controller,
+    //as these are still very much an employee-based queries in my opinion
+    //Endpoints return ResponseEntities to allow for accurate error messaging
+    @GetMapping("/reportStructure/{id}")
+    public ResponseEntity getReportingStructure(@PathVariable String id) {
         LOG.debug("Retrieving reporting structure for id [{}]", id);
 
-        return employeeService.getReportingStructure(id);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(employeeService.getReportingStructure(id));
+        }
+        catch(RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     //Employee version
-    @GetMapping("/employee/reports/{id}")
-    public Employee getEmployeeReportingStructure(@PathVariable String id) {
+    @GetMapping("/reports/{id}")
+    public ResponseEntity getEmployeeReportingStructure(@PathVariable String id) {
         LOG.debug("Retrieving reporting structure for id [{}]", id);
 
-        return employeeService.getEmployeeReportStructure(id);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(employeeService.getEmployeeReportStructure(id));
+        }
+        catch(RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/compensation")
+    public ResponseEntity createCompensation(@RequestBody Compensation compensation) {
+        LOG.debug("Received compensation create request for [{}]", compensation);
+
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(compensationService.create(compensation));
+        }
+        catch(RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/compensation/{id}")
+    public ResponseEntity readCompensation(@PathVariable String id) {
+        LOG.debug("Received compensation read request for id [{}]", id);
+
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(compensationService.read(id));
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -0,0 +1,71 @@
+package com.mindex.challenge.service.impl;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mindex.challenge.dao.CompensationRepository;
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.service.CompensationService;
+
+@Service
+public class CompensationServiceImpl implements CompensationService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CompensationServiceImpl.class);
+
+    @Autowired
+    private CompensationRepository compensationRepository;
+
+    @Override
+    public Compensation create(Compensation compensation) {
+        LOG.debug("Creating compensation [{}]", compensation);
+
+        //This logic has some grey area due to the openess of the task. I've designed it to allow for future effective dates,
+        //and then added checking to prevent from adding effective dates that come before the most recent as well as
+        //preventing dates being set in the past. A lot of this logic could change based on customer requirements,
+        //they may not need future dates, or would need the ability to delete future dates
+        if(compensation.getEffectiveDate() == null) {
+            compensation.setEffectiveDate(LocalDate.now());
+        }
+        else {
+            if(compensation.getEffectiveDate().isBefore(LocalDate.now())) {
+                throw new RuntimeException("Effective date cannot be set in the past");
+            }
+        }
+
+        List<Compensation> currentCompensations = new ArrayList<>();
+        try {
+            currentCompensations = read(compensation.getEmployeeId());
+        }
+        catch(RuntimeException e) {
+            currentCompensations = null;
+        }
+
+        if(currentCompensations != null && !currentCompensations.isEmpty()) {
+            if(currentCompensations.get(0).getEffectiveDate().isAfter(compensation.getEffectiveDate())) {
+                throw new RuntimeException("Effective date must be set after " + currentCompensations.get(0).getEffectiveDate() + " due to existing compensations");
+            }
+        }
+
+        compensationRepository.insert(compensation);
+
+        return compensation;
+    }
+
+    @Override
+    public List<Compensation> read(String employeeId) {
+        LOG.debug("Retrieving compensation for employee [{}]", employeeId);
+        List<Compensation> compensations = compensationRepository.findCompensationByEmployeeIdOrderByEffectiveDateDesc(employeeId);
+
+        if(compensations == null || compensations.isEmpty()) {
+            throw new RuntimeException("No compensations found for employee " + employeeId);
+        }
+
+        return compensations;
+    }
+}
